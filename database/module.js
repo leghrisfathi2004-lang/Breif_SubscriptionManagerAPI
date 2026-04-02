47c732cddd369c66b9d15927ff65f60d08663e6c
fathi leghris <leghrisfathi2004@gmail.com>
2026-04-02 18:05:58 +0100

    database setup

---
+++ database/module.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+//for user ----------
+const userShema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+//for subs ----------
+const subShema = new mongoose.Schema({
+    //id genere auto par mongodb
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    //true: mensuel   false: annuel
+    billingCycle: { type: Boolean, required: true },
+    createdAt: { type: Date, default: Date.now },
+    userEmail: {
+        type: String,
+        ref: 'User',
+        required: true
+    }
+})
+
+const user = mongoose.model('User', userShema);
+const sub = mongoose.model('Sub', subShema);
+
+module.exports = {user, sub};
+
